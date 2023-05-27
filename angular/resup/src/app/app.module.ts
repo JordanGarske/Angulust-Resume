579dc6637b5ace1789934c05452baa71e742c571
jordan_garske <jordago9980@yahoo.com>
2023-05-27 09:47:24 -0500

    login and signup

--- angular/resup/src/app/app.module.ts
+++ angular/resup/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { LoginComponent } from './authentication/login/login.component'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/resup/src/app/models/user.ts
@@ -0,0 +1,9 @@
+interface User {
+    client_id: number;
+    first_name: string;
+    last_name: string;
+    client_password: string;
+    email: string;
+    admin_privilege: boolean;
+  }
+  
