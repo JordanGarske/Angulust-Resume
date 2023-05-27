7a57569406695616d05e27756595c92f53524ba1
jordan_garske <jordago9980@yahoo.com>
2023-05-27 13:54:06 -0500

    f

--- rust/rustume/target/debug/rustume.d
+++ rust/rustume/target/debug/rustume.d
@@ -1,1 +1,1 @@
-D:\projects_for_programming\Angulust\rust\rustume\target\debug\rustume.exe: D:\projects_for_programming\Angulust\rust\rustume\src\main.rs
+D:\projects_for_programming\Angulust\rust\rustume\target\debug\rustume.exe: D:\projects_for_programming\Angulust\rust\rustume\src\main.rs D:\projects_for_programming\Angulust\rust\rustume\src\models.rs D:\projects_for_programming\Angulust\rust\rustume\src\schema.rs
